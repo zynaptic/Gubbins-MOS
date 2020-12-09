fcdc9e1876cd4c527cee2d32ef1ac54800a7b95c
Chris Holgate <chris@zynaptic.com>
2020-12-09 15:44:32 +0000

    Added common event flags implementation.

---
+++ common/include/gmos-events.h
@@ -0,0 +1,105 @@
+/*
+ * The Gubbins Microcontroller Operating System
+ *
+ * Copyright 2020 Zynaptic Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * This header defines the API for GubbinsMOS asynchronous event flag
+ * support.
+ */
+
+#ifndef GMOS_EVENTS_H
+#define GMOS_EVENTS_H
+
+#include <stdint.h>
+#include "gmos-scheduler.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+/**
+ * Defines the GubbinsMOS event state data structure which is used for
+ * managing a set of asynchronous event flags.
+ */
+typedef struct gmosEvent_t {
+
+    // This is a pointer to the task state data structure for the event
+    // consumer task.
+    gmosTaskState_t* consumerTask;
+
+    // This is a pointer to the next event in the pending event queue.
+    struct gmosEvent_t* nextEvent;
+
+    // This is a 32-bit integer which is used to store the 32 individual
+    // event flags as a bit field. To ensure safe operation this should
+    // only be accessed via the get, set and clear functions.
+    uint32_t eventBits;
+
+} gmosEvent_t;
+
+/**
+ * Performs a one-time initialisation of a set of GubbinsMOS event
+ * flags. This should be called during initialisation to set up the
+ * event flags for subsequent asynchronous notifications.
+ * @param event This is the event state data structure which is to be
+ *     initialised.
+ * @param consumerTask This is the consumer task which is to be notified
+ *     of any changes to the event flags.
+ */
+void gmosEventInit (gmosEvent_t* event, gmosTaskState_t* consumerTask);
+
+/**
+ * Accesses the current state of the event bits, each of which will
+ * normally be treated as an individual event flag.
+ * @param event This is the event state data structure for which the
+ *     associated event bits are being accessed.
+ * @return Returns the current state of the event bits.
+ */
+uint32_t gmosEventGetBits (gmosEvent_t* event);
+
+/**
+ * Sets one or more event bits, as specified by the bit mask. For each
+ * bit in the bit mask that is set to '1', the corresponding event bit
+ * will be set to '1'.
+ * @param event This is the event state data structure for which the
+ *     specified event bits are being modified.
+ * @return Returns the state of the event bits prior to modification.
+ */
+uint32_t gmosEventSetBits (gmosEvent_t* event, uint32_t bitMask);
+
+/**
+ * Clears one or more event bits, as specified by the bit mask. For each
+ * bit in the bit mask that is set to '1', the corresponding event bit
+ * will be set to '0'.
+ * @param event This is the event state data structure for which the
+ *     specified event bits are being modified.
+ * @return Returns the state of the event bits prior to modification.
+ */
+uint32_t gmosEventClearBits (gmosEvent_t* event, uint32_t bitMask);
+
+/**
+ * If one or more events have occurred, this function will return the
+ * associated consumer tasks in the order in which the events occured.
+ * @return Returns a reference to the next event consumer task or a null
+ *     reference if no event consumer tasks are waiting to be processed.
+ */
+gmosTaskState_t* gmosEventGetNextConsumer (void);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+#endif // GMOS_EVENTS_H
---
+++ common/src/gmos-events.c
@@ -0,0 +1,153 @@
+/*
+ * The Gubbins Microcontroller Operating System
+ *
+ * Copyright 2020 Zynaptic Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * Implements the GubbinsMOS asynchronous event flag support.
+ */
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include "gmos-platform.h"
+#include "gmos-events.h"
+
+// Specifies the head of the pending event queue.
+static gmosEvent_t* pendingEvents = NULL;
+
+// Specifies a volatile flag that can be used to avoid disabling
+// interrupts to check for queued events.
+static volatile bool pendingEventsReady = false;
+
+/*
+ * Appends an event to the end of the pending event queue if not already
+ * present in the queue.
+ */
+static void gmosEventAppendToQueue (gmosEvent_t* event)
+{
+    gmosEvent_t** nextEventPtr;
+
+    // Only append the event to the event queue if it has an associated
+    // consumer task.
+    if (event->consumerTask == NULL) {
+        return;
+    }
+
+    // Loop until the end of the queue, exiting if the queue already
+    // contains the event.
+    nextEventPtr = &pendingEvents;
+    while (*nextEventPtr != NULL) {
+        if (*nextEventPtr == event) {
+            return;
+        }
+        nextEventPtr = &((*nextEventPtr)->nextEvent);
+    }
+
+    // Append the event to the end of the queue.
+    event->nextEvent = NULL;
+    *nextEventPtr = event;
+    pendingEventsReady = true;
+}
+
+/*
+ * Performs a one-time initialisation of a set of GubbinsMOS event
+ * flags. This should be called during initialisation to set up the
+ * event flags for subsequent asynchronous notifications.
+ */
+void gmosEventInit (gmosEvent_t* event, gmosTaskState_t* consumerTask)
+{
+    event->consumerTask = consumerTask;
+    event->nextEvent = NULL;
+    event->eventBits = 0;
+}
+
+/*
+ * Accesses the current state of the event bits, each of which will
+ * normally be treated as an individual event flag.
+ */
+uint32_t gmosEventGetBits (gmosEvent_t* event)
+{
+    uint32_t eventBits = 0;
+
+    // Read the event bits with interrupts disabled.
+    gmosPalMutexLock ();
+    eventBits = event->eventBits;
+    gmosPalMutexUnlock ();
+    return eventBits;
+}
+
+/*
+ * Sets one or more event bits, as specified by the bit mask.
+ */
+uint32_t gmosEventSetBits (gmosEvent_t* event, uint32_t bitMask)
+{
+    uint32_t eventBits = 0;
+
+    // Set the event bits with interrupts disabled and then add the
+    // event to the pending queue.
+    gmosPalMutexLock ();
+    eventBits = event->eventBits;
+    event->eventBits |= bitMask;
+    gmosEventAppendToQueue (event);
+    gmosPalMutexUnlock ();
+    return eventBits;
+}
+
+/*
+ * Clears one or more event bits, as specified by the bit mask.
+ */
+uint32_t gmosEventClearBits (gmosEvent_t* event, uint32_t bitMask)
+{
+    uint32_t eventBits = 0;
+
+    // Clear the event bits with interrupts disabled and then add the
+    // event to the pending queue.
+    gmosPalMutexLock ();
+    eventBits = event->eventBits;
+    event->eventBits &= ~bitMask;
+    gmosEventAppendToQueue (event);
+    gmosPalMutexUnlock ();
+    return eventBits;
+}
+
+/*
+ * If one or more events have occurred, this function will return the
+ * associated consumer tasks in the order in which the events occured.
+ */
+gmosTaskState_t* gmosEventGetNextConsumer (void)
+{
+    gmosEvent_t* event = NULL;
+
+    // Avoid queue processing if there are no pending events.
+    if (!pendingEventsReady) {
+        return NULL;
+    }
+
+    // Pop the next event from the queue with interrupts disabled.
+    gmosPalMutexLock ();
+    if (pendingEvents != NULL) {
+        event = pendingEvents;
+        pendingEvents = event->nextEvent;
+    }
+    if (pendingEvents == NULL) {
+        pendingEventsReady = false;
+    }
+    gmosPalMutexUnlock ();
+
+    // Return the consumer task associated with the event.
+    return (event == NULL) ? NULL : event->consumerTask;
+}
