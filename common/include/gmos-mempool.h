caa1510134dc4206280e978719a665c065036f7a
Chris Holgate <chris@zynaptic.com>
2020-12-09 11:53:07 +0000

    Added common memory pool implementation.

---
+++ common/include/gmos-mempool.h
@@ -0,0 +1,105 @@
+/*
+ * The Gubbins Microcontroller Operating System
+ *
+ * Copyright 2020 Zynaptic Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * This header defines the API for the GubbinsMOS memory pool, which
+ * supports fixed sized dynamic memory allocation.
+ */
+
+#ifndef GMOS_MEMPOOL_H
+#define GMOS_MEMPOOL_H
+
+#include <stdint.h>
+#include "gmos-config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+/**
+ * Defines the GubbinsMOS memory pool segment data structure which is
+ * used for allocating a single memory pool segment.
+ */
+typedef struct gmosMempoolSegment_t {
+
+    // Specifies the location of the next segment in the list.
+    struct gmosMempoolSegment_t* nextSegment;
+
+    // Allocates a block of word aligned segment data which may be
+    // accessed as either a byte array or a 32-bit word array.
+    union {
+        uint32_t words [GMOS_CONFIG_MEMPOOL_SEGMENT_SIZE / 4];
+        uint8_t  bytes [GMOS_CONFIG_MEMPOOL_SEGMENT_SIZE];
+    } data;
+
+} gmosMempoolSegment_t;
+
+/**
+ * Initialises the memory pool. This should be called exactly once on
+ * system initialisation to set up the memory pool prior to using any
+ * other memory pool functions.
+ */
+void gmosMempoolInit (void);
+
+/**
+ * Determines the number of free memory pool segments currently
+ * available for allocation.
+ * @return Returns the number of memory pool segments currently
+ *     available for allocation.
+ */
+uint16_t gmosMempoolSegmentsAvailable (void);
+
+/**
+ * Allocates a new memory pool segment from the memory pool and returns
+ * a pointer to it.
+ * @return Returns a pointer to an allocated memory pool segment.
+ *    Returns 'NULL' if all memory pool segments have already been
+ *    allocated.
+ */
+gmosMempoolSegment_t* gmosMempoolAlloc (void);
+
+/**
+ * Returns a memory pool segment to the memory pool free list after use.
+ * @param freeSegment This is a pointer to a memory pool segment
+ *     previously allocated using 'gmosMempoolAlloc' that is to be
+ *     returned to the memory pool free list.
+ */
+void gmosMempoolFree (gmosMempoolSegment_t* freeSegment);
+
+/**
+ * Allocates a number of memory pool segments from the memory pool and
+ * returns a pointer to a linked list containing the allocated segments.
+ * @param segmentCount This is the number of memory pool segments that
+ *     are to be allocated.
+ * @return Returns a pointer to a linked list that contains the
+ *     specified number of segments, or a null reference if the
+ *     requested number of segments are not available.
+ */
+gmosMempoolSegment_t* gmosMempoolAllocSegments (uint16_t segmentCount);
+
+/**
+ * Returns a number of memory pool segments to the memory pool.
+ * @param freeSegments This is a pointer to a linked list of memory pool
+ *     segments that are to be returned to the memory pool.
+ */
+void gmosMempoolFreeSegments (gmosMempoolSegment_t* freeSegments);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+#endif // GMOS_MEMPOOL_H
---
+++ common/src/gmos-mempool.c
@@ -0,0 +1,148 @@
+/*
+ * The Gubbins Microcontroller Operating System
+ *
+ * Copyright 2020 Zynaptic Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * This common source file implements the GubbinsMOS memory pool.
+ */
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include "gmos-config.h"
+#include "gmos-platform.h"
+#include "gmos-mempool.h"
+
+// Statically allocate the memory pool area.
+static gmosMempoolSegment_t gmosMempool [GMOS_CONFIG_MEMPOOL_SEGMENT_NUMBER];
+
+// Specifies the head of the free segment list.
+static gmosMempoolSegment_t* gmosMempoolFreeList;
+
+// Specifies the number of available free segments.
+static uint16_t gmosMempoolFreeSegmentCount;
+
+/*
+ * Initialises the memory pool. This should be called exactly once on
+ * system initialisation to set up the memory pool prior to using any
+ * other memory pool functions.
+ */
+void gmosMempoolInit (void)
+{
+    uint16_t i;
+    gmosMempoolSegment_t** nextSegmentPtr;
+    gmosMempoolSegment_t* currentSegment;
+
+    // Link the memory pool free segment list.
+    nextSegmentPtr = &gmosMempoolFreeList;
+    for (i = 0; i < GMOS_CONFIG_MEMPOOL_SEGMENT_NUMBER; i++) {
+        currentSegment = &gmosMempool [i];
+        *nextSegmentPtr = currentSegment;
+        nextSegmentPtr = &(currentSegment->nextSegment);
+    }
+
+    // Add null terminator to the list.
+    *nextSegmentPtr = NULL;
+    gmosMempoolFreeSegmentCount = GMOS_CONFIG_MEMPOOL_SEGMENT_NUMBER;
+}
+
+/*
+ * Determines the number of free memory pool segments currently
+ * available for allocation.
+ */
+uint16_t gmosMempoolSegmentsAvailable (void)
+{
+    return gmosMempoolFreeSegmentCount;
+}
+
+/*
+ * Allocates a new memory pool segment from the memory pool.
+ */
+gmosMempoolSegment_t* gmosMempoolAlloc (void)
+{
+    gmosMempoolSegment_t* segment = gmosMempoolFreeList;
+    if (segment != NULL) {
+        gmosMempoolFreeList = segment->nextSegment;
+        segment->nextSegment = NULL;
+        gmosMempoolFreeSegmentCount -= 1;
+    }
+    GMOS_LOG (LOG_VERBOSE, "Memory pool alloc at 0x%08X",
+        (uint32_t)(uintptr_t) segment)
+    return segment;
+}
+
+/*
+ * Returns a memory pool segment to the memory pool free list after use.
+ */
+void gmosMempoolFree (gmosMempoolSegment_t* freeSegment)
+{
+    GMOS_LOG (LOG_VERBOSE, "Memory pool free at 0x%08X",
+        (uint32_t)(uintptr_t) freeSegment)
+    if (freeSegment != NULL) {
+        freeSegment->nextSegment = gmosMempoolFreeList;
+        gmosMempoolFreeList = freeSegment;
+        gmosMempoolFreeSegmentCount += 1;
+    }
+}
+
+/*
+ * Allocates a number of memory pool segments from the memory pool and
+ * returns a pointer to a linked list containing the allocated segments.
+ */
+gmosMempoolSegment_t* gmosMempoolAllocSegments (uint16_t segmentCount)
+{
+    uint16_t i;
+    gmosMempoolSegment_t* segment;
+    gmosMempoolSegment_t* result = NULL;
+
+    // Remove the required number of segments from the free list and
+    // null terminate the return list.
+    if (segmentCount <= gmosMempoolFreeSegmentCount) {
+        segment = gmosMempoolFreeList;
+        for (i = 1; i < segmentCount; i++) {
+            segment = segment->nextSegment;
+        }
+        result = gmosMempoolFreeList;
+        gmosMempoolFreeList = segment->nextSegment;
+        segment->nextSegment = NULL;
+        gmosMempoolFreeSegmentCount -= segmentCount;
+    }
+    return result;
+}
+
+/*
+ * Returns a number of memory pool segments to the memory pool.
+ */
+void gmosMempoolFreeSegments (gmosMempoolSegment_t* freeSegments)
+{
+    uint16_t segmentCount = 0;
+    gmosMempoolSegment_t* segment;
+
+    // Count the number of free segments and return them to the free
+    // list.
+    if (freeSegments != NULL) {
+        segmentCount = 1;
+        segment = freeSegments;
+        while (segment->nextSegment != NULL) {
+            segmentCount += 1;
+            segment = segment->nextSegment;
+        }
+        segment->nextSegment = gmosMempoolFreeList;
+        gmosMempoolFreeList = freeSegments;
+    }
+    gmosMempoolFreeSegmentCount += segmentCount;
+}
