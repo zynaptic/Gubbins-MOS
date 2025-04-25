24d7c6e11e0f0e29eff88ee82e53bdd40027b56e
Chris Holgate <chris@zynaptic.com>
2025-04-25 14:49:10 +0100

    Select MbedTLS options for OpenThread via a configuration flag.

--- platforms/silicon-labs/efr32xg2x/include/efr32-crypto-config.h
+++ platforms/silicon-labs/efr32xg2x/include/efr32-crypto-config.h
@@ -28,22 +28,26 @@
 // settings.
 #include "gmos-config.h"
 
+/**
+ * This configuration option may be used to enable the required MbedTLS
+ * cryptography libraries for OpenThread networking.
+ */
+#ifndef GMOS_CONFIG_MBEDTLS_OPENTHREAD_SUPPORT
+#define GMOS_CONFIG_MBEDTLS_OPENTHREAD_SUPPORT false
+#endif
+
 // Provide entropy source using platform specific hardware.
 #define MBEDTLS_ENTROPY_C
 #define MBEDTLS_NO_PLATFORM_ENTROPY
 #define MBEDTLS_ENTROPY_HARDWARE_ALT
 
-// Enable standard required MbedTLS features.
+// Enable commonly required MbedTLS features.
 #define MBEDTLS_BIGNUM_C
 #define MBEDTLS_AES_C
+#define MBEDTLS_SHA256_C
 #define MBEDTLS_CIPHER_C
 #define MBEDTLS_CMAC_C
 #define MBEDTLS_CTR_DRBG_C
-#define MBEDTLS_PK_C
-#define MBEDTLS_ASN1_PARSE_C
-#define MBEDTLS_ASN1_WRITE_C
-#define MBEDTLS_PK_PARSE_C
-#define MBEDTLS_OID_C
 
 // Enable PSA API support (medium profile with key storage).
 #define MBEDTLS_USE_PSA_CRYPTO
@@ -51,20 +55,33 @@
 #define MBEDTLS_PSA_CRYPTO_STORAGE_C
 #define MBEDTLS_PSA_CRYPTO_DRIVERS
 
-// Enable PAKE support.
+// Enable PAKE support and associated libraries for the OpenThread
+// joining protocol using MBEDTLS_TLS_ECJPAKE_WITH_AES_128_CCM_8.
+#if GMOS_CONFIG_MBEDTLS_OPENTHREAD_SUPPORT
+#define MBEDTLS_PK_C
+#define MBEDTLS_ASN1_PARSE_C
+#define MBEDTLS_ASN1_WRITE_C
+#define MBEDTLS_PK_PARSE_C
+#define MBEDTLS_OID_C
+#define MBEDTLS_CCM_C
 #define MBEDTLS_ECP_C
+#define MBEDTLS_MD_C
+#define MBEDTLS_HMAC_DRBG_C
 #define MBEDTLS_ECP_DP_SECP256R1_ENABLED
 #define MBEDTLS_ECJPAKE_C
 #define MBEDTLS_PK_HAVE_JPAKE
 #define MBEDTLS_PK_HAVE_CURVE_SECP256R1
 #define MBEDTLS_KEY_EXCHANGE_ECJPAKE_ENABLED
+#endif
 
-// Enable DTLS support (with TLS 1.2 as a prerequisite).
+// Enable DTLS support (with TLS 1.2 as a prerequisite) for OpenThread.
+#if GMOS_CONFIG_MBEDTLS_OPENTHREAD_SUPPORT
 #define MBEDTLS_SSL_TLS_C
 #define MBEDTLS_SSL_CLI_C
 #define MBEDTLS_SSL_EXPORT_KEYS
 #define MBEDTLS_SSL_PROTO_TLS1_2
 #define MBEDTLS_SSL_PROTO_DTLS
+#endif
 
 // Memory management functions should be explicitly provided by the
 // platform.
